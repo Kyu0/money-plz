0126286801265d4633187e46274e31ac15f0a465
Kyu0 <efftar@naver.com>
2022-07-06 22:30:05 +0900

    [new] index page 추가

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+
+const pageHandlers = require('./lib/pageHandlers')
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.engine('handlebars', engine({
+    deafultLayout: 'main'
+}))
+
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('/', pageHandlers.index)
+
+app.listen(port, () => {
+    console.log(`Server started on port : ${port}`)
+})
